c7c82f5b5aa0536a351402fec48c063d8af0792f
Juan Antonio <juanantonio.cx@gmail.com>
2022-11-12 13:10:25 -0500

    Fix inject and create products stable

--- src/main/java/com/example/springboot/programacionreactiva/ProgramacionReactivaApplication.java
+++ src/main/java/com/example/springboot/programacionreactiva/ProgramacionReactivaApplication.java
@@ -1,6 +1,10 @@
 package com.example.springboot.programacionreactiva;
 
 import com.example.springboot.programacionreactiva.models.entities.Producto;
+import com.example.springboot.programacionreactiva.models.repositories.ProductoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,20 +13,21 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class ProgramacionReactivaApplication implements CommandLineRunner {
 
+    @Autowired
+    private ProductoRepository productoRepository;
+    private static final Logger log = LoggerFactory.getLogger(ProgramacionReactivaApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(ProgramacionReactivaApplication.class, args);
     }
 
-
     @Override
-    public void run(String... args) throws Exception{
+    public void run(String... args) throws Exception {
         Flux.just(
             new Producto("Mesa Altura Regulable", 1369.89),
             new Producto("Audífono Pro X Wireless", 800.00),
-            new Producto("Mesa Altura Regulable", 1369.89)
-
-
-
-        );
+            new Producto("Iphone 13 64GB", 3000.00),
+            new Producto("Monitor Xiaomi 4K", 2800.00)
+        ).flatMap(producto -> productoRepository.save(producto))
+        .subscribe(producto -> log.info("Crea: " + producto.getId() + " nombre: " + producto.getNombre()));
     }
 }
